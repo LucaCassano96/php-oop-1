36efa00567699edcf8d1a2a5c2bcb1786e94b74d
Luca Cassano <lucacassano96@gmail.com>
2023-06-30 17:43:29 +0200

    array

--- index.php
+++ index.php
@@ -3,30 +3,33 @@
 class Movie{
 
     public $title;
-    public $genre;
+    public $genres;
     public $director;
 
-    public function __construct( $title, $genre, $director){
+    public function __construct( $title, $genres, $director){
         $this -> title = $title;
-        $this -> genre = $genre;
+        $this -> genres = $genres;
         $this -> director = $director;
     }
 
     public function getMovie(){
-        return $this -> title . " " . $this -> genre. " " . $this -> director;
+
+        $listOfGenres = implode(" ",  $this -> genres);
+
+        return $this -> title . " " . $listOfGenres . " " . $this -> director;
     }
 
 }
 
-$movie1 = new Movie('Inception','science fiction','Christopher Nolan');
+$movie1 = new Movie('Inception',["action", "sience fiction", "adventure"],'Christopher Nolan');
 /* $movie1 -> title = 'Inception';
-$movie1 -> genre = 'science fiction';
+$movie1 -> genres = 'science fiction';
 $movie1 -> director = 'Christopher Nolan'; */
 
 
-$movie2 = new Movie('The Gladiator','historical', 'Ridley Scott');
+$movie2 = new Movie('Gladiator',["action", "drama", "adventure"], 'Ridley Scott');
 /* $movie2 -> title = 'The Gladiator';
-$movie2 -> genre = 'historical';
+$movie2 -> genres = 'historical';
 $movie2 -> director = 'Ridley Scott'; */
 
 echo $movie1 -> getMovie();
