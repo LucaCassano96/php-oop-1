edb55962e36d8c73551c91309c0510e799bb2993
Luca Cassano <lucacassano96@gmail.com>
2023-06-30 16:28:00 +0200

    class first step

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+
+class Movie{
+
+    public $title;
+    public $genre;
+    public $director;
+
+
+}
+
+$movie1 = new Movie();
+$movie1 -> title = 'Inception';
+$movie1 -> genre = 'science fiction';
+$movie1 -> director = 'Christopher Nolan';
+
+
+$movie2 = new Movie();
+$movie2 -> title = 'The Gladiator';
+$movie2 -> genre = 'historical';
+$movie2 -> director = 'Ridley Scott';
